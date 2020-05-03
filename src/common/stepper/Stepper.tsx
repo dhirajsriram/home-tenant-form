f2e1ca6565fa237cda082868e1a94fa0f50f40dc
dhirajsriram3194@gmail.com <dhirajsriram3194@gmail.com>
2020-05-03 14:33:16 +0200

    Refactoring Types for stepper

--- src/common/stepper/Stepper.tsx
+++ src/common/stepper/Stepper.tsx
@@ -1,31 +1,33 @@
 import React from "react";
-import Stepper from "@material-ui/core/Stepper";
-import Step from "@material-ui/core/Step";
-import StepLabel from "@material-ui/core/StepLabel";
+import {
+  Stepper as MatStepper,
+  Step,
+  StepLabel,
+  Button,
+} from "@material-ui/core";
 import { useSelector, useDispatch } from "react-redux";
-import "./Stepper.scss";
-import { Button } from "@material-ui/core";
 import { SETSTEP } from "../../store/constants/constants";
 import { useTranslation } from "react-i18next";
+import "./Stepper.scss";
 
-function getSteps() {
-  return ["Name", "Email", "Phone", "Salary","Review", "Done"];
-}
+const getSteps = (): string[] => {
+  return ["Name", "Email", "Phone", "Salary", "Review", "Done"];
+};
 
-export default function VerticalLinearStepper() {
+const Stepper: React.FC = () => {
   const steps = getSteps();
   const { t } = useTranslation();
-  const activeStep = useSelector((state: any) => state.reducer.step);
+  const activeStep:number = useSelector((state: any) => state.reducer.step);
   const dispatch = useDispatch();
 
-  const setStep = (step: number) => {
+  const setStep = (step: number): void => {
     dispatch({ type: SETSTEP, payload: { step } });
   };
 
   return (
     <div className="stepper-container">
-      <Stepper activeStep={activeStep} orientation="vertical">
-        {steps.map((label, index) => (
+      <MatStepper activeStep={activeStep} orientation="vertical">
+        {steps.map((label:string, index:number) => (
           <Step key={index}>
             <StepLabel>
               {" "}
@@ -41,7 +43,9 @@ export default function VerticalLinearStepper() {
             </StepLabel>{" "}
           </Step>
         ))}
-      </Stepper>
+      </MatStepper>
     </div>
   );
-}
+};
+
+export default Stepper;
